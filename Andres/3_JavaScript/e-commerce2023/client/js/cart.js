57d24dfd72f8ce67aaee933dede3eafe8036e5a4
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-09-03 20:37:29 -0300

    Terminando clase 2 y 3 del ecommerce js

---
+++ Andres/3_JavaScript/e-commerce2023/client/js/cart.js
@@ -0,0 +1,104 @@
+const modalOverlay = document.getElementById("modal-overlay");
+const modalContainer = document.getElementById("modal-container");
+const cartBtn = document.getElementById("cart-btn");
+
+const displayCart = () => {
+    //Setteo de inicio
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "block";
+    modalOverlay.style.display = "block";
+    //Modal header
+    const modalHeader = document.createElement('div'); //Podemos crear lo que queramos
+    
+    const modalClose = document.createElement('div');
+    modalClose.innerText = "❌"
+    modalClose.className = "modal-close";
+    modalContainer.append(modalHeader);
+
+    // Boton cerrar
+    modalClose.addEventListener("click", ()=>{
+        modalContainer.style.display = "none";
+        modalOverlay.style.display = "none";
+    })
+
+    modalHeader.append(modalClose);
+
+    const modalTitle = document.createElement('div');
+    modalTitle.innerText = "Carrito";
+    modalTitle.className = "modal-title";
+    modalHeader.append(modalTitle);
+
+
+    // MODAL BODY
+    cart.forEach((product) => {
+        const modalBody = document.createElement('div');
+        modalBody.className = "modal-body";
+        modalBody.innerHTML = `
+        <div class="product">
+            <img class="product" src="${product.img}"/>
+            <div class="product-info">
+                <h4>${product.productName}</h4>
+        </div>
+        <div class="quantity">
+            <span class="quantity-btn-decrese">-</span>
+            <span class="quantity-input">${product.quantity}</span>
+            <span class="quantity-btn-increse">+</span>
+        </div>
+            <div class="price">${product.price * product.quantity}</div>
+            <div class="delete-product">❌</div>
+        </div>
+        `;
+
+        modalContainer.append(modalBody);
+
+        const decrese = modalBody.querySelector(".quantity-btn-decrese") //Me permite capturar elementos por su clase de css
+        decrese.addEventListener("click", ()=>{
+            if(product.quantity !== 1){
+                product.quantity--;
+                displayCart();
+            }
+            
+        })
+
+        const increse = modalBody.querySelector(".quantity-btn-increse") //Me permite capturar elementos por su clase de css
+        increse.addEventListener("click", ()=>{
+            product.quantity++;
+            displayCart();
+        })
+
+        //delete
+        const deleteProduct = modalBody.querySelector(".delete-product");
+        deleteProduct.addEventListener("click", ()=> {
+            deleteCartProduct(product.id)
+        })
+
+    })
+
+    // MODAL FOOTER
+    const total = cart.reduce((acc, el) => acc + (el.price*el.quantity), 0) // El metodo reduce trabaja con 2 parametros el primero es el acumulador y el segundo represetna cada uno de los elementos del carrito y el 3er valor es desde donde queremos que arranque =>0
+
+    const modalFooter = document.createElement('div');
+    modalFooter.className = "modal-footer";
+    modalFooter.innerHTML = `
+    <div class="total-price">Total: ${total}</div>
+    `;
+
+    modalContainer.append(modalFooter);
+
+
+}
+
+cartBtn.addEventListener("click", displayCart);
+
+//Delete product functions
+const deleteCartProduct = (id) =>{
+    const foundId = cart.findIndex((elemento) => elemento.id === id);
+    // console.log(foundId);
+    cart.splice(foundId, 1) //Elimina elementos del array mediante 2 parametros. El primero el indice y el segundo cuantos quiero eleminar
+    displayCart() //Como modifico el carrito tengo que volver a refrescar el carrito para que aparezcan los cambios
+}
+
+
+
+
+
--- Andres/3_JavaScript/e-commerce2023/client/js/index.js
+++ Andres/3_JavaScript/e-commerce2023/client/js/index.js
@@ -1,4 +1,5 @@
 const shopContent = document.getElementById("shopContent");
+const cart = []; //Este es nuestro carrito, un array vacio
 
 productos.forEach((product) =>{
     const content = document.createElement("div");
@@ -7,5 +8,30 @@ productos.forEach((product) =>{
     <h3>${product.productName}</h3>
     <p>${product.price} $</p>
     `;
+
     shopContent.append(content);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+    content.append(buyButton);
+
+    buyButton.addEventListener("click", ()=>{
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id); //Si encuentra es un true y sino es false
+        if(repeat){
+            cart.map((prod) => {
+                if(prod.id === product.id){
+                    prod.quantity++;
+                }
+            });
+        }else{
+            cart.push({
+                id:product.id,
+                productName:product.productName,
+                price:product.price,
+                quantity:product.quantity,
+                img:product.img
+            })
+        }  
+    })
 });
+
