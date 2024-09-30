62c0faca5cada404c0d912b8fea32c956a070110
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-09-29 22:29:59 -0300

    Terminamos con el proyecto estudiantes2022 con JAVA + spring

--- Andres/2_Java/Leccion13-01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes2022.java
+++ Andres/2_Java/Leccion13-01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes2022.java
@@ -14,10 +14,10 @@ import lombok.ToString;
 @NoArgsConstructor //Nos va a agregar el constructor vacio
 @AllArgsConstructor //Constructor con todos los argumentos
 @ToString //Agrega el metodo To String
-public class Estudiante {
+public class Estudiantes2022 {
     @Id //Este id es para que vaya agregando el orden de la llave primaria
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Esto es para llaves autoincrementales
-    private Integer idEstudiante;
+    private Integer idestudiantes2022;
     private String nombre;
     private String apellido;
     private String telefono;
