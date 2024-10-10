f8f6e46e935b4a33996d8c9802ae984488f4c4b4
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-10-10 00:47:01 -0300

    proyecto 2 de pygame empezado. Warning que esta lleno de errores

---
+++ Andres/1_Python/proyecto_pygame2/constantes.py
@@ -0,0 +1,15 @@
+# Dimensiones de la pantalla
+SCREEN_WIDTH = 1700
+SCREEN_HEIGTH = 1000
+
+
+# Rutas de los assets
+ASSETS_PATH = r'C:/Users/Pampa/Desktop/Tecnicatura En Programacion/2024/16 - SegundoSemestre/CodeHiveClases/Andres/1_Python/proyecto_pygame2/'
+# START_IMAGE_PATH
+# IMPERIAL_MARCH_PATH
+
+#Ruta de los archivos
+IMPERIAL_MARCH_PATH = f'{ASSETS_PATH}/sounds/imperial_march.mp3'
+START_IMAGE_PATH = f'{ASSETS_PATH}/inicio/star.png'
+ESTRELLA_PATH = f'{ASSETS_PATH}/inicio/estrella'
+FONDO1_PATH = f'{ASSETS_PATH}/imagenes/fondo1.png'
