0e4175338e317952cf06e63b4680c3957ca9911c
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-11-05 01:04:04 -0300

    Terminando con python y JAVA

--- Andres/1_Python/web-server/main.py
+++ Andres/1_Python/web-server/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() #Creamos nuestro primera instancia y con esto vamos a crear nuestro primer recuerso
+
+#Ruta principal
+@app.get('/') #Agregamos el decorador para decirle cual es la ruta
+def get_list():
+    return [1, 2, 3,]
+
+#Segunda ruta
+@app.get('/contact', response_class=HTMLResponse)
+async def get_list():
+    return """
+
+    <h1> SOY UN SITIO WEB DESDE FASTAPI </h1>
+    <p>Soy un parrafo para que me leas rey </p>
+
+"""
+
+
+
 
 """
     Prueba para entender como funciona requests la libreria de python
--- Andres/5_StakPERN/src/config.js
+++
@@ -1,1 +0,0 @@
-// COnfiguraciones de nuestra aplicacion. Usuario y contraseña de la base de datos, la direccion, puerto, toke, secret key, etc
