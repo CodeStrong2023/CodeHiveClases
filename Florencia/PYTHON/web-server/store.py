b412feb01524728cf9ee551a59aef3b735420fae
BALLADARES MARÍA FLORENCIA <111933306+Flor-balladares@users.noreply.github.com>
2024-11-10 16:59:33 -0300

    Add files via upload

---
+++ Florencia/PYTHON/web-server/store.py
@@ -0,0 +1,14 @@
+import requests
+
+def get_razas():
+    # r = requests.get('https://dog.ceo/api/breeds/list/all')
+    r = requests.get('https://dog.ceo/api/breeds/list')
+    print(r.status_code)
+    # print(r.text)
+    # Vamos a recibir un diccionario con listas
+    print(type(r.text))
+    razas = r.json()
+
+    for raza in razas.values():
+        print(f"Raza de los perritos: {raza[5]}")
+
